04749d5fe1a6f682140876843b0f287863cf7387
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-06-03 17:03:35 -0300

    :arrow_up: Building Character Device Driver

---
+++ src/kernel/char_drive.c
@@ -0,0 +1,12 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+
+static int device_open (struct inode *, struct file *);
+static int device_release (struct inode *, struct file *);
+static ssize_t device_read (struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
