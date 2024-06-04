3354bb61727680cf162ff405175d620292694f9a
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-06-04 13:34:48 -0300

    Update char_drive.c

--- src/kernel/char_drive.c
+++ src/kernel/char_drive.c
@@ -1,16 +1,1 @@
-#include <asm/io.h>
-#include <asm/uaccess.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/fs.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
-
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
-
-static struct file_operands device_fops = {
-    .read = device_read, .write = device_write, .open = device_open, .release = device_release};
 
