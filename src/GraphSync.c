cd85419c572a09d147dd3075b2bc6479e9cd378c
Yasmin <ymeira@ecomp.uefs.br>
2024-06-14 13:24:48 -0300

    feat: update GraphSync.c

--- src/GraphSync.c
+++ src/GraphSync.c
@@ -3,22 +3,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int set_sprite(int registrador, int x, int y, int offset, int activation_bit) {
-  long long int dataA = (registrador << 4) | (0000);
-  long long int dataB = (activation_bit << 29) | (x << 19) | (y << 9) | offset;
+long long int set_sprite(long long int reg, int x, int y, int offset, int activation_bit) {
+  long long int instruction = reg << 36 | activation_bit << 30 | x << 29 | y << 19 | offset;
 
-  printf("%d, %d\n", (int)dataA, (int)dataB);
-
-  long long int instruction = (dataA << 32) | (dataB);
-
-  printf("%d\n", (int)instruction);
-
-  return instruction;
+  return instruction
 }
 
 int set_background_block(int column, int line, int R, int G, int B) {}
 
-int set_background_color(int R, int G, int B) {}
+long long int set_background_color(int R, int G, int B) {
+  long long int instruction = B << 6 | G << 3 | R;
+
+  return instruction;
+}
 
 void increase_coordinate(sprite_t *sp, int mirror) {}
 
