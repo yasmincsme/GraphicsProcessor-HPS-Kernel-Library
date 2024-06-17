832ec495eeb5d153d7e2f0e55c7fad6ee21b12b0
Yasmin <ymeira@ecomp.uefs.br>
2024-06-17 16:07:13 -0300

    feat: update GraphSync.h

--- src/GraphSync.h
+++ src/GraphSync.h
@@ -7,7 +7,6 @@ typedef enum sprite_type_t;
 
 typedef enum { SPRITE_NORMAL, SPRITE_FIXED } sprite_type_t;
 
-
 typedef struct sprite_t sprite_t;
 
 struct sprite_t {
@@ -32,26 +31,43 @@ struct sprite_fixed_t {
   int ativo;         /**< Activation status of the fixed sprite. */
 };
 
+typedef struct polygon_t polygon_t;
+
+struct polygon_t {
+  u16_t ref_point_x;  /**< X-coordinate of the fixed sprite. */
+  u16_t ref_point_y;  /**< Y-coordinate of the fixed sprite. */
+  u8_t size;          /**< Offset used for bitmap selection. */
+  u8_t data_register; /**< Register where sprite information is stored. */
+  u8_t type;
+  u8_t R;
+  u8_t G;
+  u8_t B; /**< Activation status of the fixed sprite. */
+};
+
+typedef struct ground_block_t ground_block_t;
+
+struct ground_block_t {
+  u64_t column;
+  u64_t line;
+  u8_t R;
+  u8_t G;
+  u8_t B;
+};
+
 void write_data(u64_t fd, u64_t data);
 
 u64_t read_data(u64_t fd);
 
-//uint64_t set_background_block(int column, int line, int R, int G, int B);
+u64_t set_background_block(ground_block_t block);
+
+u64_t set_fixed_sprite(sprite_fixed_t sprite);
+
+u64_t set_dynamic_sprite(sprite_t sprite);
 
 u64_t set_background_color(u8_t R, u8_t G, u8_t B);
+
+u64_t set_polygon(polygon_t polygon);
 
 void increase_coordinate(sprite_t *sp, int mirror);
 
 int collision(sprite_t *sp1, sprite_t *sp2);
-
-//void write_DP(u8_t type, u8_t R, u8_t G, u8_t B, u8_t lenght, u16_t x, u16_t y, u16_t adress);
-
-//void write_WSM(u8_t R, u8_t G, u8_t B, u32_t adress);
-
-//void write_WBR_sprite(u8_t ativo, u16_t x, u16_t y, u16_t offset, u8_t reg);
-
-//void write_WBR_background(u8_t R, u8_t G, u8_t B);
-
-u64_t set_polygon(int reg, int format, int R, int G, int B, int size, int ref_point_y, int ref_point_x);
-
-u64_t set_sprite(u8_t reg, u8_t sp, u16_t x, u16_t y, u16_t offset);
