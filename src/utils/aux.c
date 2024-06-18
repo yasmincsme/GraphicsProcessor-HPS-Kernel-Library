02ed991f5c1ae1dec51d5957a6072caec4cccb11
Yasmin <ymeira@ecomp.uefs.br>
2024-06-18 06:42:42 -0300

    feat: update aux

--- src/utils/aux.c
+++ src/utils/aux.c
@@ -47,3 +47,9 @@ char* itoa(u32_t num, char* str, u32_t base) {
 
   return str;
 }
+
+int clamp(int value, int min, int max) {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
+}
