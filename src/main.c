0601cc8bafa2c0473d868eae94d3f454aea0086a
Yasmin Cordeiro <67525293+yasmincsme@users.noreply.github.com>
2024-06-18 16:53:14 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,50 +10,192 @@
 
 int main(void) {
 
-  u64_t instruction1 = set_background_color(7, 5, 1);
+  //BACKGROUND
+  u64_t instruction1 = set_background_color(4, 5, 6);
 
-  sprite_fixed_t sprite;
-  sprite.ativo = 1;
-  sprite.data_register = 2;
-  sprite.coord_x = 320;
-  sprite.coord_y = 120;
-  sprite.offset = 18;
-
-  set_fixed_sprite(sprite);
-
+  //BACKGROUND BLOCKS
   ground_block_t block;
-  block.address = 4;
-  block.R = 6;
-  block.G = 0;
-  block.B = 0; //677
+  block.address = 3000;
+  block.R = 1;
+  block.G = 4;
+  block.B = 1;
 
-  // set_background_block(block);
+  int i = 3600;
 
-  // int j, i = 0;
+  for (i; i < 4800; i++) {
+    block.address = i;
+    set_background_block(block);
+  }
 
+  // //SPRITE SPACESHIP
+  // sprite_fixed_t spaceship_1;
+  // spaceship_1.ativo = 0;
+  // spaceship_1.data_register = 1;
+  // spaceship_1.coord_x = 190;
+  // spaceship_1.coord_y = 190;
+  // spaceship_1.offset = 5;
 
-  //   for(j; j<4799; j++) {
-  //     block.address = j;
- 
-  //     set_background_block(block);
-  //   }
+  // set_fixed_sprite(spaceship_1);
 
+  // //SPRITE SPACESHIP
+  // sprite_fixed_t spaceship_2;
+  // spaceship_2.ativo = 0;
+  // spaceship_2.data_register = 2;
+  // spaceship_2.coord_x = 50;
+  // spaceship_2.coord_y = 50;
+  // spaceship_2.offset = 5;
 
-    // // printf("\n%x\n", instruction1);
+  // set_fixed_sprite(spaceship_2);
 
-    // u64_t instruction2 =
-    //     set_polygon(2, 1, 7, 2, 7, 2, 40,
-    //                 40);  // int reg, int format, int R, int G, int B, int size, int ref_point_y, int ref_point_x
-    // printf("\n%x\n", instruction2);
+  // //SPRITE ASTEROIDES
+  // sprite_fixed_t asteroid_1;
+  // spaceship_2.ativo = 0;
+  // spaceship_2.data_register = 3;
+  // spaceship_2.coord_x = 250;
+  // spaceship_2.coord_y = 250;
+  // spaceship_2.offset = 23;
 
-    // u64_t instruction3 =
-    //     set_sprite(6, 1, 100, 100, 4);  // u64_t set_sprite(u8_t reg, u8_t sp, u16_t x, u16_t y, u16_t offset)
-    // printf("\n%x\n", instruction3);
+  // set_fixed_sprite(spaceship_2);
 
-    // uint64_t write_value_background = set_background_color(5, 2, 3);
-    // uint64_t write_value_sprite = set_sprite(2, 10, 10, 1, 1);
-    // uint64_t write_value_background_block = set_background_block(10, 10, 2, 2, 7);
-    // uint64_t write_value_polygon = set_polygon(3, 1, 4, 5, 2, 5, 60, 60);
+  //BASE DO CASTELO
+  polygon_t polygon_1;
+  polygon_1.data_register = 14;
+  polygon_1.type = 0;
+  polygon_1.size = 14;
+  polygon_1.ref_point_x = 450;
+  polygon_1.ref_point_y = 320;
+  polygon_1.R = 5;
+  polygon_1.G = 3;
+  polygon_1.B = 4;
 
-    return 0;
+  set_polygon(polygon_1);
+
+  //TORRES DO CASTELO
+
+  //TORRE 1
+  polygon_t pol_tower_1;
+  pol_tower_1.data_register = 5;
+  pol_tower_1.type = 0;
+  pol_tower_1.size = 2;
+  pol_tower_1.ref_point_x = 390;
+  pol_tower_1.ref_point_y = 235;
+  pol_tower_1.R = 5;
+  pol_tower_1.G = 3;
+  pol_tower_1.B = 4;
+
+  set_polygon(pol_tower_1);
+
+  polygon_t pol_tower_11;
+  pol_tower_11.data_register = 6;
+  pol_tower_11.type = 0;
+  pol_tower_11.size = 2;
+  pol_tower_11.ref_point_x = 390;
+  pol_tower_11.ref_point_y = 220;
+  pol_tower_11.R = 5;
+  pol_tower_11.G = 3;
+  pol_tower_11.B = 4;
+
+  set_polygon(pol_tower_11);
+
+  //TORRE 2
+  polygon_t pol_tower_2;
+  pol_tower_2.data_register = 7;
+  pol_tower_2.type = 0;
+  pol_tower_2.size = 2;
+  pol_tower_2.ref_point_x = 510;
+  pol_tower_2.ref_point_y = 235;
+  pol_tower_2.R = 5;
+  pol_tower_2.G = 3;
+  pol_tower_2.B = 4;
+
+  set_polygon(pol_tower_2);
+
+  polygon_t pol_tower_22;
+  pol_tower_22.data_register = 8;
+  pol_tower_22.type = 0;
+  pol_tower_22.size = 2;
+  pol_tower_22.ref_point_x = 510;
+  pol_tower_22.ref_point_y = 220;
+  pol_tower_22.R = 5;
+  pol_tower_22.G = 3;
+  pol_tower_22.B = 4;
+
+  set_polygon(pol_tower_22);
+
+  //TORRE 3
+  polygon_t pol_tower_3;
+  pol_tower_3.data_register = 9;
+  pol_tower_3.type = 0;
+  pol_tower_3.size = 2;
+  pol_tower_3.ref_point_x = 450;
+  pol_tower_3.ref_point_y = 235;
+  pol_tower_3.R = 5;
+  pol_tower_3.G = 3;
+  pol_tower_3.B = 4;
+
+  set_polygon(pol_tower_3);
+
+  polygon_t pol_tower_33;
+  pol_tower_33.data_register = 10;
+  pol_tower_33.type = 0;
+  pol_tower_33.size = 2;
+  pol_tower_33.ref_point_x = 450;
+  pol_tower_33.ref_point_y = 210;
+  pol_tower_33.R = 5;
+  pol_tower_33.G = 3;
+  pol_tower_33.B = 4;
+
+  set_polygon(pol_tower_33);
+
+  //TELHADOS
+  polygon_t pol_tel_1;
+  pol_tel_1.data_register = 11;
+  pol_tel_1.type = 1;
+  pol_tel_1.size = 3;
+  pol_tel_1.ref_point_x = 390;
+  pol_tel_1.ref_point_y = 185;
+  pol_tel_1.R = 3;
+  pol_tel_1.G = 1;
+  pol_tel_1.B = 1;
+
+  set_polygon(pol_tel_1);
+
+  polygon_t pol_tel_2;
+  pol_tel_2.data_register = 12;
+  pol_tel_2.type = 1;
+  pol_tel_2.size = 3;
+  pol_tel_2.ref_point_x = 510;
+  pol_tel_2.ref_point_y = 185;
+  pol_tel_2.R = 3;
+  pol_tel_2.G = 1;
+  pol_tel_2.B = 1;
+
+  set_polygon(pol_tel_2);
+
+  polygon_t pol_tel_3;
+  pol_tel_3.data_register = 13;
+  pol_tel_3.type = 1;
+  pol_tel_3.size = 5;
+  pol_tel_3.ref_point_x = 450;
+  pol_tel_3.ref_point_y = 165;
+  pol_tel_3.R = 3;
+  pol_tel_3.G = 1;
+  pol_tel_3.B = 1;
+
+  set_polygon(pol_tel_3);
+
+  //PORTA DO CASTELO
+  polygon_t pol_door;
+  pol_door.data_register = 4;
+  pol_door.type = 0;
+  pol_door.size = 5;
+  pol_door.ref_point_x = 450;
+  pol_door.ref_point_y = 365;
+  pol_door.R = 3;
+  pol_door.G = 1;
+  pol_door.B = 1;
+
+  set_polygon(pol_door);
+
+  return 0;
 }
