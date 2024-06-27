c22a584f48221a93426f06d5f75e9533a1b075ac
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-06-27 14:48:08 -0300

    Update makingSprite.py

--- src/makingSprite.py
+++ src/makingSprite.py
@@ -3,3 +3,5 @@ from PIL import Image
 spritePixels = Imagem.open("path")
 
 rgbTest = spritePixels.getPixel(00)
+
+#https://imasters.com.br/back-end/primeiros-passos-com-pil-a-biblioteca-de-imagens-do-python
