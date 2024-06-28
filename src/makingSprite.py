6c57d6e19074f74bfadbf2097bd7583f7579804e
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-06-28 11:56:02 -0300

    Update makingSprite.py

--- src/makingSprite.py
+++ src/makingSprite.py
@@ -2,6 +2,14 @@ from PIL import Image
 
 spritePixels = Imagem.open("path")
 
-rgbTest = spritePixels.getPixel(00)
+
+matrizRGB = []
+rgb = []
+
+for line in range(20):
+  for col in range(20):
+    coordinates = line, col
+    rgbTest = spritePixels.getPixel(coordinates)
+    print(rgbTest)
 
 #https://imasters.com.br/back-end/primeiros-passos-com-pil-a-biblioteca-de-imagens-do-python
