557d1486fd902886125b3215ccaced2bc6727834
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-07-01 14:54:42 -0300

    Update makingSprite.py

--- src/makingSprite.py
+++ src/makingSprite.py
@@ -1,8 +1,6 @@
 from PIL import Image
 
 spritePixels = Image.open("docs\cactus-sprite-pixilart.png")
-px = spritePixels.load()
-px[4, 4] = (0, 0, 0)
 
 matrizRGB = []
 rgb = []
@@ -11,8 +9,19 @@ for line in range(20):
   for col in range(20):
     coordinates = line, col
     rgbValues = spritePixels.getpixel(coordinates)
-    print(rgbValues[:3])
+    #print(rgbValues[:3])
     rgb.append(rgbValues[:3])
 
+transparentColor = [6, 7, 7]
+
 for color in rgb:
-  
+  if color == (0, 0, 0):
+    matrizRGB.append(transparentColor)
+  else:
+    newColor = []
+    for number in color:
+      convertedRGB = int(number/32)
+      newColor.append(convertedRGB)
+    matrizRGB.append(newColor)
+
+print(matrizRGB)
