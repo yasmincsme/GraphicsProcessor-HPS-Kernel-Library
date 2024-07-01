b819d03b4309856afd5ede4f7befbe09acf3e4e5
Nathielle Alves <93337467+NathielleA@users.noreply.github.com>
2024-07-01 14:28:58 -0300

    feat: making sprite

--- src/makingSprite.py
+++ src/makingSprite.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
-spritePixels = Imagem.open("path")
-
+spritePixels = Image.open("docs\cactus-sprite-pixilart.png")
+px = spritePixels.load()
+px[4, 4] = (0, 0, 0)
 
 matrizRGB = []
 rgb = []
@@ -9,7 +10,9 @@ rgb = []
 for line in range(20):
   for col in range(20):
     coordinates = line, col
-    rgbTest = spritePixels.getPixel(coordinates)
-    print(rgbTest)
+    rgbValues = spritePixels.getpixel(coordinates)
+    print(rgbValues[:3])
+    rgb.append(rgbValues[:3])
 
-#https://imasters.com.br/back-end/primeiros-passos-com-pil-a-biblioteca-de-imagens-do-python
+for color in rgb:
+  
